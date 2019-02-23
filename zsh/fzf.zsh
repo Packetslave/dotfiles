a12f7db716cb8c6f106db49e699e17e2737f2c0d
Brian Landers <blanders@iMac-Pro.packetslave.local>
2019-02-22 21:56:48 -0800

    updates

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,14 +1,11 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/blanders/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/blanders/.fzf/bin"
-fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/blanders/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf.completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/blanders/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf.key-bindings.zsh"
 
