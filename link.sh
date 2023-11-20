f53b1ec63ff94edc98baa278b81273e287c01a92
Brian Landers <brian@packetslave.com>
2023-11-20 11:29:56 -0800

    link script

---
+++ link.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -sf ~/dotfiles/zsh/p10k.zsh ~/.p10k.zsh
+ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/tmux/tmuxline ~/.tmuxline
+
+mkdir -p ~/.emacs.d
+ln -sf ~/dotfiles/emacs/emacs.d/init.el ~/.emacs.d/init.el
