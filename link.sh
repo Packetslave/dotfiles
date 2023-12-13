943544e0970184c7cec4519ef3d4822ff6bcbada
Brian Landers <brian.landers@reddit.com>
2023-12-13 11:47:44 -0800

    new vim config

--- link.sh
+++ link.sh
@@ -4,8 +4,11 @@ ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -sf ~/dotfiles/zsh/p10k.zsh ~/.p10k.zsh
 ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/tmux/tmuxline ~/.tmuxline
+ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim/vim ~/.vim
 
 mkdir -p ~/.emacs.d
 ln -sf ~/dotfiles/emacs/emacs.d/init.el ~/.emacs.d/init.el
 
+rm ~/bin
 ln -sf ~/dotfiles/bin ~/bin
